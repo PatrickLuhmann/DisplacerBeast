498238c02fbe2daf736b0a69dedef74cdb1d656c
Patrick Luhmann <patrick.luhmann@gmail.com>
2016-08-31 18:09:32 -0700

    First commit
    Just some basic code showing how to manipulate a TextBox.

---
+++ DisplacerBeast/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace DisplacerBeast
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainOutputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // MainOutputTextBox
+            // 
+            this.MainOutputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainOutputTextBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainOutputTextBox.Location = new System.Drawing.Point(0, 0);
+            this.MainOutputTextBox.Multiline = true;
+            this.MainOutputTextBox.Name = "MainOutputTextBox";
+            this.MainOutputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.MainOutputTextBox.Size = new System.Drawing.Size(801, 607);
+            this.MainOutputTextBox.TabIndex = 0;
+            this.MainOutputTextBox.Text = "Hello World";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 607);
+            this.Controls.Add(this.MainOutputTextBox);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MainOutputTextBox;
+    }
+}
+
---
+++ DisplacerBeast/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisplacerBeast
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // PJL code
+            this.MainOutputTextBox.Text += "This is a test.";
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.AppendLine("This is line number 1.");
+            sb.AppendLine("This is line number 2.");
+            this.MainOutputTextBox.Text += sb.ToString();
+            int val = 123;
+            sb.Clear();
+            sb.AppendFormat("{0:X4} == {0}", val, val);
+            sb.AppendLine();
+            this.MainOutputTextBox.Text += sb.ToString();
+
+            for (int x = 0; x < 32; x++)
+            {
+                this.MainOutputTextBox.Text += string.Format("{0,2}", x);
+                this.MainOutputTextBox.Text += System.Environment.NewLine;
+            }
+
+            System.Diagnostics.Debug.WriteLine("This is a debug test.");
+
+        }
+    }
+}
